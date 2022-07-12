c6c85b1e58cd073abf37c1d11b49f7f75df941e8
Andrei <vitobravis@gmail.com>
2022-07-12 13:18:19 +0700

    fix Component

--- src/app/js/base/component.ts
+++ src/app/js/base/component.ts
@@ -24,8 +24,8 @@ abstract class Component {
         this.nRootName = name;
     }
 
-    public getElement = <T extends HTMLElement>(name: string): T | null => {
-        return this.nRoot.querySelector(`.${this.nRootName}__${name}`);
+    public getElement = <T extends HTMLElement>(name: string): T | undefined => {
+        return this.nRoot.querySelector<T>(`.${this.nRootName}__${name}`) ?? undefined;
     };
 
     public getElements = <T extends HTMLElement>(name: string): T[] => {
