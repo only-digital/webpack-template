954a68a58db5f382592eff6316de8341183cdbce
Anastasia Vyaltseva <anastasiavyalt@gmail.com>
2022-07-27 16:42:43 +0300

    split webpack config to prod & dev configs
    todo: sync swc & ts config

---
+++ webpack-config/webpack.common.ts
@@ -0,0 +1,174 @@
+import { Configuration } from 'webpack';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
+import SpriteLoaderPlugin from 'svg-sprite-loader/plugin';
+import CopyPlugin from 'copy-webpack-plugin';
+import path from 'path';
+import colors from 'colors';
+import getPages from './pages';
+import createDataJson from './dataJson';
+
+console.log(`[ ${colors.green.bold('START')} ] Сборка проекта\n`);
+
+const pages = getPages();
+
+console.log('');
+
+const dataJson = createDataJson();
+
+console.log('');
+
+const config: Configuration =  {
+    entry: {
+        common: './src/app/js/common.ts',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                use: [
+                    {
+                        loader: 'html-loader',
+                        options: {
+                            attributes: {
+                                list: [
+                                    {
+                                        tag: 'img',
+                                        attribute: 'src',
+                                        type: 'src',
+                                    },
+                                    {
+                                        tag: 'video',
+                                        attribute: 'src',
+                                        type: 'src',
+                                    },
+                                    {
+                                        tag: 'video',
+                                        attribute: 'data-src',
+                                        type: 'src',
+                                    },
+                                    {
+                                        tag: 'picture',
+                                        attribute: 'src',
+                                        type: 'src',
+                                    },
+                                    {
+                                        tag: 'source',
+                                        attribute: 'srcset',
+                                        type: 'src',
+                                    },
+                                    {
+                                        tag: 'source',
+                                        attribute: 'src',
+                                        type: 'src',
+                                    },
+                                    {
+                                        tag: 'link',
+                                        attribute: 'href',
+                                        type: 'src',
+                                    },
+                                ],
+                            },
+                        },
+                    },
+                    {
+                        loader: 'pug-html-loader',
+                        options: {
+                            data: { dataJson },
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|avif|webp|gif|svg)$/i,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: 'assets/images/[name].[hash:8].[ext]',
+                    },
+                },
+                include: [path.join(__dirname, '..', 'src', 'assets', 'images')],
+            },
+            {
+                test: /\.(cur)$/i,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: 'assets/images/[name].[ext]',
+                    },
+                },
+                include: [path.join(__dirname, '..', 'src', 'assets', 'icons')],
+            },
+            {
+                test: /\.(svg|png)$/i,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: 'assets/favicons/[name].[ext]',
+                    },
+                },
+                include: [path.join(__dirname, '..', 'src', 'assets', 'favicons')],
+            },
+            {
+                test: /\.(mp4|avi)$/i,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: 'assets/videos/[name].[ext]',
+                    },
+                },
+            },
+            {
+                test: /\.(woff2|woff|ttf|otf)$/i,
+                use: {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: 'assets/fonts',
+                        publicPath: './../assets/fonts',
+                    },
+                },
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: 'svg-sprite-loader',
+                        options: {
+                            outputPath: 'assets/sprites',
+                        },
+                    },
+                    'svg-transform-loader',
+                ],
+                include: [path.join(__dirname, '..', 'src', 'assets', 'icons')],
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.pug'],
+        alias: {
+            "@/helpers": path.resolve(__dirname, '..', 'src', 'app', 'js', 'helpers'),
+            "@/base": path.resolve(__dirname, '..', 'src', 'app', 'js', 'base'),
+            "@/variables": path.resolve(__dirname, '..', 'src', 'app', 'js', 'variables'),
+            "@/types": path.resolve(__dirname, '..', 'src', 'app', 'js', 'types'),
+            "@/components": path.resolve(__dirname, '..', 'src', 'components'),
+            "@/pages": path.resolve(__dirname,  '..','src', 'pages')
+        }
+    },
+    target: ['web', 'es5'],
+    plugins: [
+        ...pages,
+        new SpriteLoaderPlugin({ plainSprite: true }) as { apply(...args: any[]): void; },
+        new CleanWebpackPlugin(),
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: 'dev',
+                    noErrorOnMissing: true,
+                },
+            ],
+        }),
+        // new BundleAnalyzerPlugin()
+    ],
+};
+
+export default config;
---
+++ webpack-config/webpack.prod.ts
@@ -0,0 +1,60 @@
+import { Configuration } from 'webpack';
+import { merge } from 'webpack-merge';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import path from 'path';
+import commonConfig from './webpack.common';
+
+const productionConfig: Configuration = {
+    mode: 'production',
+    output: {
+        filename: 'js/[name].[chunkhash].js',
+        path: path.resolve(__dirname, '..', 'build'),
+        publicPath: './',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+            },
+            {
+                test: /\.m?js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [['@babel/preset-env', { targets: 'ie 11' }]],
+                    },
+                },
+                exclude: /node_modules[\/\\](?!(swiper|dom7|load-script2)[\/\\])/,
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                config: path.resolve(__dirname, '..', 'postcss.config.js'),
+                            },
+                        },
+                    },
+                    {
+                        loader: 'sass-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[chunkhash].css',
+            chunkFilename: 'css/[id].css',
+        }),
+    ],
+};
+
+export default merge(commonConfig, productionConfig);
